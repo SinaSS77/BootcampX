6d5c9c34c5367921ec098b73a7291836ce26df05
Sina Molaverdikhani <s.molaverdikhani@gmail.com>
2023-02-07 11:00:35 -0800

    a simple file to practice SELECT in SELECT

---
+++ 4_SELECT_in_SELECT/example.sql
@@ -0,0 +1,22 @@
+-- SELECT
+--   (
+--     SELECT
+--       count(assignments)
+--     FROM
+--       assignments
+--   ) - count(assignment_submissions) as total_incomplete
+-- FROM
+--   assignment_submissions
+--   JOIN students ON students.id = student_id
+-- WHERE
+--   students.name = 'Ibrahim Schimmel';
+
+--   SELECT count(students)
+-- FROM students
+-- JOIN cohorts on cohorts.id = cohort_id
+-- GROUP BY cohorts;
+
+SELECT avg(count(students))
+FROM students
+JOIN cohorts on cohorts.id = cohort_id
+GROUP BY cohorts
