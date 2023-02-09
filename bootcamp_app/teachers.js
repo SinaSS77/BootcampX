90486cc1a67d29019f1d284bdaeb6d0852eb7d8a
Sina Molaverdikhani <s.molaverdikhani@gmail.com>
2023-02-08 21:49:20 -0800

    Get an arg from commandline and connect to database, find and return it

---
+++ bootcamp_app/teachers.js
@@ -0,0 +1,32 @@
+
+
+const { Pool } = require('pg');
+
+const pool = new Pool({
+  user: 'vagrant',
+  password: '123',
+  host: 'localhost',
+  database: 'bootcampx'
+});
+
+const cohortName = process.argv[2];
+// const limit = process.argv[3] || 5;
+// Store all potentially malicious values in an array. 
+const values = [`%${cohortName}%`];
+
+const queryString = `
+SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort
+FROM teachers
+JOIN assistance_requests ON teacher_id = teachers.id
+JOIN students ON student_id = students.id
+JOIN cohorts ON cohort_id = cohorts.id
+WHERE cohorts.name LIKE $1
+ORDER BY teacher
+`;
+
+pool.query(queryString, values)
+.then(res => {
+  res.rows.forEach(row => {
+    console.log(`${row.cohort}: ${row.teacher}`);
+  })
+}).catch(err => console.error('query error', err.stack));
